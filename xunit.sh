88ee36b59f0702bd4ae85d0b75dde1bb4689e874
Yann Crumeyrolle <murk@club-internet.fr>
2014-07-09 23:17:59 +0200

    xs

--- xunit.sh
+++ xunit.sh
@@ -3,7 +3,7 @@
 mono --runtime=v4.0 .nuget/NuGet.exe install xunit.runners -Version 2.0.0-beta-build2700 -o packages
 
 runTest(){
-    mono --runtime=v4.0 packages/xunit.runners.2.0.0-beta-build2700/tools/xunit.console.exe $@ -xml
+    mono --runtime=v4.0 packages/xunit.runners.2.0.0-beta-build2700/tools/xunit.console.exe $@ -xml build/coverage.xml
    if [ $? -ne 0 ]
    then   
      exit 1
